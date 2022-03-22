0c6489e00e91b4a70ca3fa1e8f3ef7e5f0141c29
DimK19 <kyriakidis_dimitrios@outlook.com>
2022-03-22 20:46:20 +0200

    Fix student creation controller and endpoint URL
    and add error 500 page

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, abort
 from flask_mysqldb import MySQL
 from flaskr import app, db
 
@@ -39,24 +39,25 @@ def getStudents():
         return render_template("students.html", students = students, pageTitle = "Students Page")
     except Exception as e:
         print(e)
+        abort(500)
 
-@app.route("/student-creation-page")
-def studentCreationPage():
-    return render_template("create_student.html", pageTitle = "Create Student")
-
-@app.route("/create-student", methods = ["POST"]) ## "GET" by default
+@app.route("/students/create", methods = ["GET", "POST"]) ## "GET" by default
 def createStudent():
-    newStudent = request.form
-    query = "INSERT INTO students(name, surname, email) VALUES ('{}', '{}', '{}');".format(newStudent['name'], newStudent['surname'], newStudent['email'])
-    try:
-        cur = db.connection.cursor()
-        cur.execute(query)
-        db.connection.commit()
-        cur.close()
-        flash("Student inserted successfully", "success")
-    except Exception as e: ## OperationalError
-        flash(str(e), "danger")
-    return redirect(url_for("index"))
+    if(request.method == "POST"):
+        newStudent = request.form
+        query = "INSERT INTO students(name, surname, email) VALUES ('{}', '{}', '{}');".format(newStudent['name'], newStudent['surname'], newStudent['email'])
+        try:
+            cur = db.connection.cursor()
+            cur.execute(query)
+            db.connection.commit()
+            cur.close()
+            flash("Student inserted successfully", "success")
+        except Exception as e: ## OperationalError
+            flash(str(e), "danger")
+        return redirect(url_for("index"))
+
+    ## else, response for GET request
+    return render_template("create_student.html", pageTitle = "Create Student")
 
 @app.route("/grades")
 def getGrades():
@@ -68,9 +69,14 @@ def getGrades():
         cur.close()
         return render_template("grades.html", grades = grades, pageTitle = "Grades Page")
     except Exception as e:
+        abort(500)
         print(e)
 
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
-    return render_template("404.html", pageTitle = "Not Found"), 404
+    return render_template("errors/404.html", pageTitle = "Not Found"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("errors/500.html", pageTitle = "Internal Server Error"), 500
