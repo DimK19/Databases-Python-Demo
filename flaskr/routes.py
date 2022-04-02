6c7d7bdec115f6d5454daa0b4eb7a3e39a67cd31
DimK19 <kyriakidis_dimitrios@outlook.com>
2022-04-02 15:56:21 +0300

    Add input validation to student update endpoint
    and comments

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -7,8 +7,12 @@ from flaskr.forms import StudentForm
 @app.route("/")
 def index():
     try:
+        ## create connection to database
         cur = db.connection.cursor()
+        ## execute query
         cur.execute("SELECT g.grade, s.name, s.surname FROM students s INNER JOIN grades g ON g.student_id = s.id WHERE g.course_name = 'DRI' ORDER BY g.grade DESC LIMIT 1")
+        ## cursor.fetchone() does not return the column names, only the row values
+        ## thus we manually create a mapping between the two, the dictionary res
         column_names = [i[0] for i in cur.description]
         res = dict(zip(column_names, cur.fetchone()))
         best_dribbling_grade = res.get("grade")
@@ -16,6 +20,7 @@ def index():
 
         cur.execute("SELECT g.grade, s.name, s.surname FROM students s INNER JOIN grades g ON g.student_id = s.id WHERE g.course_name = 'SHO' ORDER BY g.grade DESC LIMIT 1")
         res = dict(zip(column_names, cur.fetchone()))
+        cur.close()
         best_shooting_grade = res.get("grade")
         best_shooter = res.get("name") + " " + res.get("surname")
 
@@ -29,9 +34,11 @@ def index():
         print(e)
         return render_template("landing.html", pageTitle = "Landing Page")
 
-
 @app.route("/students")
 def getStudents():
+    """
+    Retrieve students from database
+    """
     try:
         form = StudentForm()
         cur = db.connection.cursor()
@@ -46,7 +53,11 @@ def getStudents():
 
 @app.route("/students/create", methods = ["GET", "POST"]) ## "GET" by default
 def createStudent():
+    """
+    Create new student in the database
+    """
     form = StudentForm()
+    ## when the form is submitted
     if(request.method == "POST" and form.validate_on_submit()):
         newStudent = form.__dict__
         query = "INSERT INTO students(name, surname, email) VALUES ('{}', '{}', '{}');".format(newStudent['name'].data, newStudent['surname'].data, newStudent['email'].data)
@@ -65,21 +76,32 @@ def createStudent():
 
 @app.route("/students/update/<int:id>", methods = ["POST"])
 def updateStudent(id):
+    """
+    Update a student in the database, by id
+    """
     form = StudentForm()
     updateData = form.__dict__
-    query = "UPDATE students SET name = '{}', surname = '{}', email = '{}' WHERE id = {};".format(updateData['name'].data, updateData['surname'].data, updateData['email'].data, id)
-    try:
-        cur = db.connection.cursor()
-        cur.execute(query)
-        db.connection.commit()
-        cur.close()
-        flash("Student updated successfully", "success")
-    except Exception as e:
-        flash(str(e), "danger")
+    if(form.validate_on_submit()):
+        query = "UPDATE students SET name = '{}', surname = '{}', email = '{}' WHERE id = {};".format(updateData['name'].data, updateData['surname'].data, updateData['email'].data, id)
+        try:
+            cur = db.connection.cursor()
+            cur.execute(query)
+            db.connection.commit()
+            cur.close()
+            flash("Student updated successfully", "success")
+        except Exception as e:
+            flash(str(e), "danger")
+    else:
+        for category in form.errors.values():
+            for error in category:
+                flash(error, "danger")
     return redirect(url_for("getStudents"))
 
 @app.route("/grades")
 def getGrades():
+    """
+    Retrieve grades from database
+    """
     try:
         cur = db.connection.cursor()
         cur.execute("SELECT * FROM grades")
@@ -93,7 +115,9 @@ def getGrades():
 
 @app.route("/grades/delete/<int:id>", methods = ["POST"])
 def deleteGrade(id):
-    updateData = request.form
+    """
+    Delete grade by id from database
+    """
     query = f"DELETE FROM grades WHERE id = {id};"
     try:
         cur = db.connection.cursor()
