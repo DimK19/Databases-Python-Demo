032f996b4a679df07fc4b79cbef417350e66eaf9
DimK19 <kyriakidis_dimitrios@outlook.com>
2022-04-02 17:02:32 +0300

    Actually correct forms

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -3,10 +3,10 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 class StudentForm(FlaskForm):
-    name = StringField(label = "Name", validators = [DataRequired(message = "Required field.")])
+    name = StringField(label = "Name", validators = [DataRequired(message = "Name is a required field.")])
 
-    surname = StringField(label = "Surname", validators = [DataRequired(message = "Required field.")])
+    surname = StringField(label = "Surname", validators = [DataRequired(message = "Surname is a required field.")])
 
-    email = StringField(label = "Email", validators = [DataRequired(message = "Required field."), Email(message = "Invalid email format")])
+    email = StringField(label = "Email", validators = [DataRequired(message = "Email is a required field."), Email(message = "Invalid email format.")])
 
     submit = SubmitField("Create")
